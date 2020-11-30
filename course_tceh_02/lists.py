5b841ec61254b67c881dce77897a5ecae4c4d536
wowblvck <indar.basto@gmail.com>
2020-11-30 21:10:19 +0300

    Add more functions in lists.py

--- course_tceh_02/lists.py
+++ course_tceh_02/lists.py
@@ -1,5 +1,5 @@
 # lists:
-not_a_list = (1, 2, )
+not_a_list = (1, 2,)
 list1 = [1, 2]
 list2 = list(not_a_list)
 
@@ -25,4 +25,28 @@ new_var = var
 var.append('foo')
 var[0] = 'baz'
 print(new_var)
-#end
+# end
+
+# delete:
+test_list = ['pop', 'remove', 'del']
+
+# by index:
+popped = test_list.pop(0)
+print('popped value is "{}"'.format(popped))
+
+# by value:
+test_list.remove('remove')
+
+# or del it:
+del test_list[0]
+
+multi = [
+    [0, 1, 2, 3],
+    [4, 5, 6, 7],
+    [8, 9, 10, 11]
+]  # matrix 4*3
+
+for row in multi:
+    print(row)
+    for element in row:
+        print('element: ', element)
