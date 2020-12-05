6f988a2b968776368a601c9ebf46b3ecf1cce3bd
wowblvck <indar.basto@gmail.com>
2020-12-05 14:11:42 +0300

    Add more scripts in errors_code.py

--- course_tceh_03/errors_code.py
+++ course_tceh_03/errors_code.py
@@ -19,3 +19,20 @@ except TypeError:
     print('Come on. Warning TypeError')
 except RuntimeError:
     print('Come on. Warning RuntimeError')
+
+
+def list_int(*args):
+    x = 0
+    for a in args:
+        if type(a) == int:
+            x += 1
+    if x < len(args):
+        raise ValueError
+    else:
+        print(tuple(sorted(args)))
+
+
+try:
+    list_int(5, 12, -1, 8, 34)
+except ValueError:
+    print('Ti dolboeb')
