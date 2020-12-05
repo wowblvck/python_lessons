98f8ff683b315c85aa2f90622c743333e299cada
wowblvck <indar.basto@gmail.com>
2020-12-05 14:48:04 +0300

    Edit code

--- course_tceh_03/homework.py
+++ course_tceh_03/homework.py
@@ -1,12 +1,10 @@
-def get_index(x):
-    new_list = ['hi!', 'salam', 12, 64, 'goodboy']
-    print(new_list[x])
-
-
+new_list = ['hi!', 'salam', 12, 64, 'goodboy']
 try:
     my_get_index = int(input('Give index: '))
-    get_index(my_get_index)
-except ValueError as ve:
-    print('Error: ', ve)
-except IndexError as ie:
-    print('Error: ', ie)s
+    if my_get_index >= 0 and my_get_index >= len(new_list):
+        raise IndexError
+    print(new_list[my_get_index])
+except ValueError:
+    print('Неверное значение')
+except IndexError:
+    print('Неверный индекс')
